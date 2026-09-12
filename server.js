af8e4498647ae730cabc164c32bfd215e675a89c
rutherfordium-104 <jesschristiandecastrosanico@gmail.com>
2026-09-12 22:17:13 +0800

    Update server.js

--- server.js
+++ server.js
@@ -146,7 +146,7 @@ app.get("/study/new-advanced", (req, res) => {
 })
 
 app.get("/documents", (req, res) => {
-    sendFileOrError(res, '/files/documents', 'files/documents.html');
+    sendFileOrError(res, '/documents/filesupload', 'documents/filesupload.html');
 })
 
 // in your server.js
