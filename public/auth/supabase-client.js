9f020ba737e2656530ad6fe80e591dafebca79ee
roshaunangelia-jpg <roshaunangelia@gmail.com>
2026-09-22 08:56:17 +0800

    Roshaun's GitHub compiler | pushed at: 2026-09-22 08:56:17

--- public/auth/supabase-client.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://YOUR-PROJECT-REF.supabase.co'
-const supabaseAnonKey = 'YOUR-ANON-KEY'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
